68951e9a32b29e9c9d188e1d40d37956c6dc1de0
Agapothur <arthur.hollebeque@outlook.com>
2026-03-11 15:22:15 +0100

    refactor

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -7,56 +7,74 @@ class GildedRose {
         this.items = items;
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
+    public void doBrie(int i){
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1;
+        }
 
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
+        items[i].sellIn = items[i].sellIn - 1;
+
+        if (items[i].sellIn < 0) {
+            if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1;
+            }
+        }
+    }
+
+    public void doPass(int i){
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1;
+
+            if (items[i].sellIn < 11) {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (items[i].sellIn < 6) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+        }
+
+        items[i].sellIn = items[i].sellIn - 1;
+
+
+        if (items[i].sellIn < 0) {
+            items[i].quality = items[i].quality - items[i].quality;
+        }
+    }
+
+    public void doSulfuras(int i){
+        if (items[i].sellIn < 0) {
+            items[i].quality = items[i].quality - items[i].quality;
+        }
+    }
+
+    public void doOther(int i){
+        if (items[i].quality > 0) {
+            items[i].quality = items[i].quality - 1;
+        }
+
+        items[i].sellIn = items[i].sellIn - 1;
+
+        if (items[i].sellIn < 0) {
+            if (items[i].quality > 0) {
+                    items[i].quality = items[i].quality - 1;
+
+            }
+        }
+    }
+
+    public void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
+            switch (items[i].name) {
+                case "Aged Brie" -> doBrie(i);
+                case "Sulfuras, Hand of Ragnaros" -> doSulfuras(i);
+                case "Backstage passes to a TAFKAL80ETC concert" -> doPass(i);
+                case null, default -> doOther(i);
             }
         }
     }
