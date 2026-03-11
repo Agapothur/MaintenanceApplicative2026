6e018e35e54f3e70f2c2c85b9d74cc04bcf8b76c
Agapothur <arthur.hollebeque@outlook.com>
2026-03-11 14:49:42 +0100

    tests

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,43 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testNone(){
+        Item[] items = new Item[] { new Item("None", 0, 1) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("None", app.items[0].name);
+    }
+
+    @Test
+    void testNone2(){
+        Item[] items = new Item[] { new Item("None", 0, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("None", app.items[0].name);
+    }
+
+    @Test
+    void testAgedBrie(){
+         Item[] items = new Item[] { new Item("Aged Brie", 0, 0) };
+         GildedRose app = new GildedRose(items);
+         app.updateQuality();
+         assertEquals("Aged Brie", app.items[0].name);
+    }
+
+    @Test
+    void testBackstagePasses(){
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+    }
+
+    @Test
+    void testSulfuras(){
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
     }
 
 }
